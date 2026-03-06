8e345fda66c8150b9720bdef3cdc366ba37a3301
Ephemeral <ephemeral.those316@slmails.com>
2026-03-06 22:51:13 +0600

    fix(nix): fix the nix package build

--- nix/package.nix
+++ nix/package.nix
@@ -1,33 +1,19 @@
 {
   buildGoModule,
-  fetchFromGitHub,
   installShellFiles,
   lib,
-  makeBinaryWrapper,
   stdenv,
   versionCheckHook,
-  # TODO: remove wl-clipboard dependencies
-  wl-clipboard,
 }:
 buildGoModule rec {
   pname = "yankd";
-  version = "0.0.1-dev.4";
+  version = "0.0.1-unstable-2025-03-06";
 
-  src = fetchFromGitHub {
-    owner = "Nadim147c";
-    repo = "yankd";
-    rev = "21447b8107c18b8470955ba80fca40ab7427e740";
-    hash = "sha256-/iDG83YGeR/bx1sB1O5nOqQLQzB6pG7nWfHmS9tnlAQ=";
-  };
+  src = ../.;
 
-  vendorHash = "sha256-qmKm1Y4q43hWRdF1leT+2UujX9VlBJmpP51rxhpnBc4=";
+  vendorHash = "sha256-6YVIFvV0p7uZvVkuOx1tYOn+xRZ9AqKpNqm4xbWzqo8=";
 
-  nativeBuildInputs = [
-    installShellFiles
-    makeBinaryWrapper
-  ];
-  propagatedBuildInputs = [ wl-clipboard ];
-
+  nativeBuildInputs = [ installShellFiles ];
   nativeInstallCheckInputs = [ versionCheckHook ];
   versionCheckProgramArg = "--version";
 
@@ -36,16 +22,12 @@ buildGoModule rec {
       --bash <($out/bin/yankd _carapace bash) \
       --fish <($out/bin/yankd _carapace fish) \
       --zsh <($out/bin/yankd _carapace zsh)
-
-    wrapProgram $out/bin/yankd \
-      --prefix PATH : ${lib.makeBinPath [ wl-clipboard ]}
   '';
 
   ldflags = [
     "-s"
     "-w"
-    "-X"
-    "main.version=${version}"
+    "-X main.version=${version}"
   ];
 
   meta = {
