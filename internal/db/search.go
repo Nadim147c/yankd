79833d969bfb477d48f6c08a1b536f6e172a9f42
Ephemeral <ephemeral.those316@slmails.com>
2026-03-10 00:41:50 +0600

    feat(search): make full-text fuzzy by default

--- internal/db/search.go
+++ internal/db/search.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"log/slog"
 	"strings"
 
 	"github.com/Nadim147c/yankd/internal/models"
@@ -18,15 +19,40 @@ func (db *DB) Search(ctx context.Context, query string, limit int) ([]models.Cli
 		return db.GetLast(ctx, int64(limit))
 	}
 
+	if stringContainsAny(query, "*", "AND", "OR", "NOT") {
+		return db.fullTextSearch(ctx, query) // The query contains sqlite search oparator
+	}
+
+	fields := strings.Fields(query)
+	for i, field := range fields {
+		fields[i] = field + "*"
+	}
+
+	// add the *<word>* to make the serach fuzzy
+	return db.fullTextSearch(ctx, strings.Join(fields, " "))
+}
+
+func (db *DB) fullTextSearch(ctx context.Context, query string) ([]models.ClipboardEvent, error) {
+	slog.Info("sqlite full-text search", "query", query)
 	res, err := db.queries.FullTextSearch(ctx, query)
 	if err != nil {
 		return nil, err
 	}
 
 	ids := make([]int64, len(res))
+	_ = ids[len(res)-1] // no runtime bound check on the loop
 	for i := range res {
 		ids[i] = res[i].ID
 	}
 
 	return db.GetMany(ctx, ids)
+}
+
+func stringContainsAny(s string, parts ...string) bool {
+	for i := range parts {
+		if strings.Contains(s, parts[i]) {
+			return true
+		}
+	}
+	return false
 }
